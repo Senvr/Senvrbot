90b38a30436ee0f35da5e8776f7d23218f26aefe
Lee Ikard <Leeeyecard@gmail.com>
2018-08-23 19:02:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.voice_client import VoiceClient
 prefix = "$"
 
 bot = commands.Bot(command_prefix=prefix)
-TOKEN = 'NDgyMDc3NzIyMTY0NzIzNzEz.DmA49A.-7wd0vV-wUN2fwBn7IfyioM_cKs'
+TOKEN = ''
   # Where 'TOKEN' is your bot token
 @bot.event
 async def on_ready():
