f35c89ced25361d51dc9cd4b34768f184674436e
Lee Ikard <Leeeyecard@gmail.com>
2018-08-26 20:17:06 -0700

    0.3-1
    added screen line
    you dont have to use this script

--- starter.sh
+++ starter.sh
@@ -2,7 +2,8 @@
 #while true; do  sleep 1;        echo "1";       cat ~/pipy_bot/log.log; done &
 cd ~/pipy_bot/
 
-python3 ~/pipy_bot/main.py 
+
+screen -A -m -d -S PyBot python3 ~/pipy_bot/main.py 
 
 #while true; do  sleep 1;        echo "1";       cat ~/pipy_bot/log.log; done &
 
