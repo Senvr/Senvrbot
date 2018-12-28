2d5a41177cc74bcf814d49c3229115e5c373a85a
Senvr <Leeeyecard@gmail.com>
2018-12-28 13:33:34 -0800

    Update install.sh
    thorn wasn't needed, anyways

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ sudo apt update
 sudo apt install libssl-dev  -y
 sudo apt install python3 python3-pip libffi-dev -y
 sudo apt install -y libsqlite3-dev
-sudo pip3 install thorn
+#sudo pip3 install thorn
+#Down, apparently?
 sudo apt install libopus0 ffmpeg
 sudo python3 -m pip install -U discord.py[voice] 
 sudo python3 -m pip install -U discord.py
