bf2dae699c78af121a3d217776eab7a719937b89
Senvr <Leeeyecard@gmail.com>
2018-12-28 13:15:48 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 sudo apt update
-set -e
-#required crap
-sudo apt install libssl-dev  -y
+sudo apt install libssl-dev screen -y
 sudo apt install python3 python3-pip libffi-dev -y
 sudo apt install -y libsqlite3-dev
-pip3 install thorn
+sudo pip3 install thorn
 sudo apt install libopus0 ffmpeg
 sudo python3 -m pip install -U discord.py[voice] 
 sudo python3 -m pip install -U discord.py
 sudo pip3 install tinytag
-echo "Done."
+echo "Done. If there were any errors, then try installing those packages manually."
