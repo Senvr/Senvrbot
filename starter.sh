0c42de8d3a6e4156cebf35699ba75c522b3d1181
Senvr <Leeeyecard@gmail.com>
2018-12-28 13:45:14 -0800

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,2 +1,3 @@
-cd DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
 screen -S SenvrBot1 -d -m python3 main.py
