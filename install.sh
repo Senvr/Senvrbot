c3749dbfd6f1b4334264bd96c8aefd90d9ccea77
Senvr <Leeeyecard@gmail.com>
2018-12-28 13:14:07 -0800

    did some gc

--- install.sh
+++ install.sh
@@ -1,21 +1,11 @@
 sudo apt update
 set -e
-#required crap
 sudo apt install libssl-dev  -y
 sudo apt install python3 python3-pip libffi-dev -y
 sudo apt install -y libsqlite3-dev
-pip3 install thorn
-if [ $1 == "audio" ]; then
-	sudo apt install libopus0 ffmpeg
-	sudo python3 -m pip install -U discord.py[voice] 
-fi
-
+sudo pip3 install thorn
+sudo apt install libopus0 ffmpeg
+sudo python3 -m pip install -U discord.py[voice] 
 sudo python3 -m pip install -U discord.py
-
-#extra features: 
 sudo pip3 install tinytag
-
-#done
 echo "Done."
-
-
